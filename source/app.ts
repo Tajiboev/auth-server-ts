bea2c66d35961be5e59ce5f46a30bbe3a72ba501
Tajiboev <mukhammadjon.tajiboev@gmail.com>
2021-07-12 17:25:39 +0500

    added helmet, moved db connect code to server.ts

--- source/app.ts
+++ source/app.ts
@@ -1,31 +1,20 @@
 import express from 'express';
-import morgan from 'morgan';
 import cors from 'cors';
+import { errorHandler, notFound } from './errorHandler';
 
 import authRoutes from './api/routes/auth';
 import projectRoutes from './api/routes/projects';
+import helmet from 'helmet';
 import compression from 'compression';
-import { errorHandler, notFound } from './errorHandler';
 
 const app = express();
 
-// Connect DB
-import('./connectDB');
-
-app.use(
-	compression({
-		level: 6,
-		threshold: 1024,
-		filter: (req, res) => {
-			if (req.headers['x-no-compression']) return false;
-			return compression.filter(req, res);
-		}
-	})
-);
+app.disable('etag');
+app.use(helmet());
 app.use(cors());
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(morgan('dev'));
 
 app.use('/auth', authRoutes);
 app.use('/projects', projectRoutes);
