e35ce9e49d492f506714415a492b8b5d6aff5fc7
Tajiboev <mukhammadjon.tajiboev@gmail.com>
2021-07-12 17:38:39 +0500

    fixed connection issue

--- source/server.ts
+++ source/server.ts
@@ -1,40 +1,30 @@
 import { createServer } from 'https';
 import { serverConfig, mongo } from './config';
 import app from './app';
-import mongoose, { connection, connect } from 'mongoose';
+import mongoose from 'mongoose';
 
 const server = createServer(serverConfig.ssl, app);
 
 server.listen(serverConfig.port, () => {
 	console.info(`Server listening on port ${serverConfig.port}`);
 });
-connect(mongo.url, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true,
-	useFindAndModify: false
-})
-	.then((r) => console.info(r))
+mongoose
+	.connect(mongo.url, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false
+	})
+	.then((r) => console.info('Mongoose connected'))
 	.catch((e) => console.error(e));
 
 const stopServer = async () => {
 	server.close();
 	await mongoose.disconnect();
-	await connection.close();
+	await mongoose.connection.close();
+	console.info('Server shut down!');
 	process.exit(0);
 };
-
-connection.on('connected', () => {
-	console.info('Mongoose connected');
-});
-
-connection.on('error', (err) => {
-	console.error(err.message);
-});
-
-connection.on('disconnected', () => {
-	console.info('Mongoose disconnected');
-});
 
 process.on('SIGINT', stopServer);
 process.on('SIGTERM', stopServer);
