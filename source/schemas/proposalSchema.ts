73783d9cb9c780beda57059de0b4f2a9a1f04b4d
Tajiboev <mukhammadjon.tajiboev@gmail.com>
2021-07-19 15:12:13 +0500

    presave and postremove middleware

--- source/schemas/proposalSchema.ts
+++ source/schemas/proposalSchema.ts
@@ -1,4 +1,7 @@
-import { Schema } from 'mongoose';
+import { Schema, HookNextFunction } from 'mongoose';
+import { IProposalDocument } from '../interfaces/proposal';
+import Project from '../models/projectModel';
+import User from '../models/userModel';
 
 const proposalSchema: Schema = new Schema(
 	{
@@ -6,14 +9,26 @@ const proposalSchema: Schema = new Schema(
 		message: String,
 		author: {
 			type: Schema.Types.ObjectId,
-			ref: 'user'
+			ref: 'User',
+			required: true
 		},
 		project: {
 			type: Schema.Types.ObjectId,
-			ref: 'project'
+			ref: 'Project',
+			required: true
 		}
 	},
 	{ strictQuery: true, timestamps: true }
 );
+
+proposalSchema.pre('save', async function (this: IProposalDocument, next: HookNextFunction) {
+	await User.updateOne({ _id: this.author }, { $addToSet: { proposals: [this._id] } }).exec();
+	await Project.updateOne({ _id: this.project }, { $addToSet: { proposals: [this._id] } }).exec();
+});
+
+proposalSchema.post('remove', async function (this: IProposalDocument, next: HookNextFunction) {
+	await User.updateOne({ _id: this.author }, { $pull: { propasals: this._id } }).exec();
+	await Project.updateOne({ _id: this.project }, { $pull: { propasals: this._id } }).exec();
+});
 
 export default proposalSchema;
