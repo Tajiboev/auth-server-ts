3c59e3faf31a187feef32fa3ebc08dc72a962460
Tajiboev <mukhammadjon.tajiboev@gmail.com>
2021-07-19 15:13:53 +0500

    reject on null token

--- source/utils/jwt.ts
+++ source/utils/jwt.ts
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 const signJWT = (id: string): Promise<Error | string> => {
 	return new Promise((resolve, reject) => {
 		jwt.sign({ uid: id }, jwtSecrets.token_secret, { expiresIn: '1d' }, (err, token) => {
-			if (err) reject(new Error('Error signing JWT'));
+			if (err || !token) reject(new Error('Error signing JWT'));
 			if (token) resolve(token);
 		});
 	});
