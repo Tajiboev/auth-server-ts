47f1c6df945ca7e12e580c4497e8eeae1cbf875f
Tajiboev <mukhammadjon.tajiboev@gmail.com>
2021-08-05 22:36:12 +0500

    healthcheck

--- source/app.ts
+++ source/app.ts
@@ -1,27 +1,31 @@
 import express from 'express';
 import cors from 'cors';
-import { errorHandler, notFound } from './errorHandler';
-
-import authRoutes from './routes/auth';
-import projectRoutes from './routes/projects';
 import helmet from 'helmet';
 import compression from 'compression';
-import morgan from 'morgan';
+
+import { errorHandler, notFound } from './errorHandler';
+
+import authRouter from './routers/auth';
+import projectRouter from './routers/projects';
 
 const app = express();
 
 app.disable('etag');
-app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(compression());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/auth', authRoutes);
-app.use('/projects', projectRoutes);
+app.use('/auth', authRouter);
+app.use('/projects', projectRouter);
 
-//errors
+// healthcheck
+app.get('/healthcheck', async (req, res) => {
+	res.sendStatus(200);
+});
+
+//error handler
 app.use(notFound);
 app.use(errorHandler);
 
