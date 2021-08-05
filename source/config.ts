fdfb64baab782cff96f01a92783e905443a032b4
Tajiboev <mukhammadjon.tajiboev@gmail.com>
2021-08-05 22:36:49 +0500

    rethinking config

--- source/config.ts
+++ source/config.ts
@@ -1,25 +1,10 @@
 import dotenv from 'dotenv';
-import fs from 'fs';
 
 const config = dotenv.config();
 if (config.error || !config.parsed) throw config.error;
 const env = config.parsed;
 
-const mongo = {
-	url: `mongodb+srv://${env.MONGO_USERNAME}:${env.MONGO_PASSWORD}@cluster0.9oliw.mongodb.net/${env.MONGO_DBNAME}?retryWrites=true&w=majority`
-};
+const dbUrl = `mongodb+srv://${env.MONGO_USERNAME}:${env.MONGO_PASSWORD}@cluster0.9oliw.mongodb.net/${env.MONGO_DBNAME}?retryWrites=true&w=majority`;
+const port = env.PORT;
 
-const serverConfig = {
-	hostname: env.SERVER_HOSTNAME || 'localhost',
-	port: env.SERVER_PORT || 5000,
-	ssl: {
-		key: fs.readFileSync(__dirname + '/ssl/privateKey.key'),
-		cert: fs.readFileSync(__dirname + '/ssl/certificate.crt')
-	}
-};
-
-const jwtSecrets = {
-	token_secret: env.JWT_ACCESS_TOKEN_SECRET || 'supersecretkey'
-};
-
-export { serverConfig, mongo, jwtSecrets };
+export { dbUrl, port };
